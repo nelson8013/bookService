70e9915a50d2d115e014716cc3d79412b630e1ef
nelson8013 <nelsonekpenyong@gmail.com>
2020-09-19 11:49:58 +0100

    react router setup with LoginPage

--- src/index.js
+++ src/index.js
@@ -2,13 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import serviceWorker from './serviceWorker'
+import registerServiceWorker from "./registerServiceWorker";
 
 ReactDOM.render(
   <BrowserRouter>
-    <App/>
-  </BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
 );
 
-serviceWorker();
+registerServiceWorker();
+
 
